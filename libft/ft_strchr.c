942e7e9692c12d1cbe579254f57f715900e68b4f
Christiaan Herrewijn <cherrewi@f0r3s9.codam.nl>
2023-04-11 11:53:13 +0200

    setup file structure

---
+++ libft/ft_strchr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strchr.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: cherrewi <cherrewi@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/19 15:12:09 by cherrewi      #+#    #+#                 */
+/*   Updated: 2022/10/19 15:12:10 by cherrewi      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+Locates the first occurrence of c (converted to a char) in string s.
+returns NULL if the character does not appear in the string
+*/
+char	*ft_strchr(const char *s, int c)
+{
+	if ((char)c == 0)
+	{
+		while (*s)
+			s++;
+		return ((char *)s);
+	}
+	else
+	{
+		while (*s)
+		{
+			if (*s == (char)c)
+				return ((char *)s);
+			s++;
+		}
+	}
+	return (NULL);
+}
