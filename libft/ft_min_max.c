54cf80e27b1435ea2b6ed33f1da1a705eeebb023
Christiaan Herrewijn <cherrewi@f0r3s9.codam.nl>
2023-05-01 23:43:02 +0200

    libft linting fix

--- libft/ft_min_max.c
+++ libft/ft_min_max.c
@@ -6,20 +6,21 @@
 /*   By: cherrewi <cherrewi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/27 22:48:40 by cherrewi      #+#    #+#                 */
-/*   Updated: 2023/04/27 22:51:09 by cherrewi      ########   odam.nl         */
+/*   Updated: 2023/05/01 23:42:34 by cherrewi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_max(size_t i, size_t j)
+size_t	ft_max(size_t i, size_t j)
 {
 	if (i > j)
 		return (i);
 	else
 		return (j);
 }
-size_t ft_min(size_t i, size_t j)
+
+size_t	ft_min(size_t i, size_t j)
 {
 	if (i < j)
 		return (i);
